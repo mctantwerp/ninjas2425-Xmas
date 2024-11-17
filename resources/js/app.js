7d5079642078971eb0fd5a51e6f4bef32ffcd0d9
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-17 17:39:36 +0100

    vue init + test component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import test from './components/hangman/test.vue';
+
+app.component('test', test);
+
+app.mount('#app'); // Ensure your Blade template has an element with this ID
+
---
+++ resources/js/components/hangman/test.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>lol</p>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped></style>
