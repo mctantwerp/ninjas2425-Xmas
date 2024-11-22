223b81524f5555126f728bb8a5a9e80754ca0852
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-22 22:31:17 +0100

    changed content of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,9 @@
 
 @section('content')
     <div class="window-intro">
-        <h1>Ho ho ho! Merry Christmas!</h1>
-        <p>Welcome to ...</p>
+        <h1>Welcome to the 2024 KDG Christmas Challenge! 🎄</h1>
+        <p>This holiday season, embark on a scavenger hunt across the KDG campuses! Find posters with QR codes and secret
+            numbers, solve the challenges, and collect the words. Got them all? Submit your answers for a chance to win
+            amazing prizes! Good luck and have fun with the challenge! 🎅✨</p>
     </div>
 @endsection
