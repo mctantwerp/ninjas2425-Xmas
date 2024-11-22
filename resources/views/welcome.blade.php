5e8b1a290e3d75655771eb7a659175ed9e236d2c
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-23 00:01:37 +0100

    welcome page content changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,12 @@
 @section('content')
     <div class="window-intro">
         <h1>Welcome to the 2024 KDG Christmas Challenge! 🎄</h1>
-        <p>This holiday season, embark on a scavenger hunt across the KDG campuses! Find posters with QR codes and secret
-            numbers, solve the challenges, and collect the words. Got them all? Submit your answers for a chance to win
+        <p>This holiday season, embark on a scavenger hunt across the KDG campuses!</p>
+        </br>
+        <p>Find posters with QR codes and secret
+            numbers, solve the challenges, and collect the words.</p>
+        </br>
+        <p>Got them all? Submit your answers for a chance to win
             amazing prizes! Good luck and have fun with the challenge! 🎅✨</p>
     </div>
 @endsection
