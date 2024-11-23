664b35f1d509640ecc6d0337c9a0d77e6ab60167
tobias <tobias.oostvogels1@gmail.com>
2024-11-23 12:55:00 +0100

    downgrading packages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin'; // Check if using default export works
 
 export default defineConfig({
     plugins: [
-        laravel({
+        laravel.default({ // Use default if laravel is a CommonJS module
             input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/sass/welcomePage.scss'],
             refresh: true,
         }),
