1a17fdd6c494c16b5a98c459c7851ea0fab262c5
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-24 15:52:37 +0100

    added shake if user clicks wrong image

--- resources/js/components/search/searchGame.vue
+++ resources/js/components/search/searchGame.vue
@@ -1,17 +1,45 @@
 <template>
-    <div class="image-container">
+    <div class="image-container" @click="handleClick" ref="shakeElement">
         <img src="../assets/search-background.png"
             alt="search game background image with many icons like presents and others" class="image" />
-        <div class="overlay-box" @click="correct"></div>
+        <div class="overlay-box"></div>
     </div>
 </template>
 
 <script>
+import { gsap } from "gsap";
 export default {
     methods: {
         correct() {
             this.$bus.emit('correct');
-        }
+        },
+        handleClick() {
+            //if target contains overlay-box class, emit event
+            if (!event.target.classList.contains('overlay-box')) {
+                console.log('overlay-box clicked');
+                this.triggerShake();
+            }
+            else {
+                this.correct();
+            }
+        },
+        async triggerShake() {
+            //animation
+            await gsap.to(this.$refs.shakeElement, {
+                duration: 0.1,
+                x: 6,
+                y: 6,
+                repeat: 4,
+                yoyo: true,
+                ease: "power2.inOut",
+            });
+            gsap.to(this.$refs.shakeElement, {
+                duration: 0.1,
+                x: 0,
+                y: 0,
+                ease: "power2.inOut",
+            });
+        },
     }
 }
 </script>
