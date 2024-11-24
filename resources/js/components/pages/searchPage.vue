7f81bc9705edc8c454856349484b4330761509c3
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-24 15:24:15 +0100

    added search game, fully functional

---
+++ resources/js/components/pages/searchPage.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="game-container">
+        <transition name="fade-with-slide" mode="out-in">
+            <first-popup v-if="!gameStarted" @game-start="startGame" :game="'search'">
+                <template v-slot:content>
+                    <h2>Christmas search</h2>
+                    <p>Find Santa's frozen present and reveal one of the words for the Christmas Challenge. BTF41</p>
+                </template>
+                <template v-slot:action>Continue</template>
+            </first-popup>
+
+            <search-game v-else-if="gameStarted && !correctAnswer"></search-game>
+
+            <final-popup v-else>
+                <template v-slot:content>
+                    <h2>Congrats!</h2>
+                    <p>The word in this game is 'MCT'. Complete the other challenges to reveal the entire sentence. Good
+                        luck!</p>
+                </template>
+                <template v-slot:action><button @click="redirectSubmitPage">Submit sentence</button></template>
+            </final-popup>
+        </transition>
+    </div>
+</template>
+
+
+<script>
+import confetti from "canvas-confetti";
+export default {
+    data() {
+        return {
+            gameStarted: false,
+            isDesktop: window.innerWidth > 1024,
+            correctAnswer: false,
+        }
+    },
+    computed: {
+        viewportWidth() {
+            return window.innerWidth;
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.checkViewport);
+        this.$bus.on('correct', () => {
+            console.log('Correct word! Event listener works!');
+            this.correctAnswer = true;
+            this.triggerConfetti();
+        });
+    },
+    methods: {
+        triggerConfetti() {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 },
+            });
+        },
+        startGame() {
+            this.gameStarted = true;
+            document.getElementsByClassName('snow')[0].style.display = "none";
+            document.getElementsByClassName('gingerman')[0].style.display = "none";
+            document.getElementsByClassName('candy')[0].style.display = "none";
+            document.getElementsByClassName('footer')[0].style.display = "none";
+        },
+        checkViewport() {
+            this.isLargeViewport = window.innerWidth > 1024;
+            console.log(this.isLargeViewport);
+        },
+        redirectSubmitPage() {
+            window.location.href = '/submit';
+        }
+    },
+    beforeUnmount() {
+        // Remove event listener when the component is destroyed
+        window.removeEventListener('resize', this.checkViewport);
+        this.$bus.off('correct');
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.game-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    width: 100%;
+}
+</style>
---
+++ resources/js/components/search/searchGame.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="image-container">
+        <img src="../assets/search-background.png"
+            alt="search game background image with many icons like presents and others" class="image" />
+        <div class="overlay-box" @click="correct"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        correct() {
+            this.$bus.emit('correct');
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.image-container {
+    position: relative;
+    display: inline-block;
+}
+
+.image {
+    display: block;
+    width: 100%;
+    height: auto;
+}
+
+.overlay-box {
+    position: absolute;
+    bottom: 130px;
+    right: 32px;
+    width: 70px;
+    height: 70px;
+    cursor: pointer;
+    z-index: 10;
+}
+</style>
