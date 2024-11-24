321be3ca70a2876a555b868ba8534c571d96651e
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-24 23:25:35 +0100

    vite changes, deletion of bootstrap.js file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/sass/welcomePage.scss'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/sass/welcomePage.scss', 'resources/js/shakeDecoration.js'],
             refresh: true,
         }),
         vue(),
