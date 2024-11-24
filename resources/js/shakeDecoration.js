e567dbc14ba6c5b3a193b900e6b9c50cac3ceba5
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-24 22:59:30 +0100

    shake happens automatically as well from now on, it repeats a shake regardless of it being clicked or not.

--- resources/js/shakeDecoration.js
+++ resources/js/shakeDecoration.js
@@ -2,13 +2,31 @@ import { gsap } from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
     const elements = document.querySelectorAll(".snow, .gingerman, .candy");
+    const snow = document.querySelector(".snow");
+    const gingerman = document.querySelector(".gingerman");
+    const candy = document.querySelector(".candy");
+
     elements.forEach(element => {
         element.addEventListener("click", async () => {
             await triggerShake(element);
         });
     });
-});
 
+    const triggerShakeLoop = async () => {
+        await triggerShake(snow);
+        await delay(2500);
+        await triggerShake(gingerman);
+        await delay(3250);
+        await triggerShake(snow);
+        await delay(750);
+        await triggerShake(candy);
+
+        setTimeout(triggerShakeLoop, 2500); //repeat every 7 seconds
+    };
+
+    //start loop
+    triggerShakeLoop();
+});
 
 async function triggerShake(element) {
     await gsap.to(element, {
@@ -24,3 +42,8 @@ async function triggerShake(element) {
         ease: "power2.inOut",
     });
 }
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
