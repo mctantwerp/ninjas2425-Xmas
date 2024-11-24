5c8e26b44fddfce0cd342e7741c3fdcd8ab9aaa0
tobias <tobias.oostvogels1@gmail.com>
2024-11-24 23:57:45 +0100

    added the word stripes

--- resources/js/components/hangman/searchHangman.vue
+++ resources/js/components/hangman/searchHangman.vue
@@ -5,12 +5,12 @@
                 <div class="box">
                     <img src="../assets/hangman/hangman-1.png">
                 </div>
-                <div class="word">
-                    <span class="letter" v-for="(letter, i) in letters" :key="i">
-                        {{  correctLetters.includes(letter) ? letter : '' }}
-                    </span>
+            </div>
+            <div class="word">
+                <span class="letter" v-for="(letter, i) in letters" :key="i">
+                    {{  correctLetters.includes(letter) ? letter : '' }}
+                </span>
 
-                </div>
             </div>
         </div>
     </div>
@@ -19,8 +19,14 @@
 <script>
 export default {
     props: {
-        letters: "christmas",
-        correctLetters: { type: Array, default: () => [] }
+        letters: {
+            type: Array, // Ensure the type matches the expected data structure
+            default: () => ['c', 'h', 'r', 'i', 's', 't', 'm', 'a', 's'] // Default value is an array of letters
+        },
+        correctLetters: {
+            type: Array,
+            default: () => []
+        }
     },
     data() {
         return {
@@ -45,39 +51,51 @@ export default {
 <style lang="scss" scoped>
 @import "/resources/sass/_variables.scss";
 
-.wrapper {
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
-    width: 100%;
+.hangman-wrapper{
+    position: absolute;
+    top: 50;
 
-    .box-wrapper {
+    .wrapper {
         display: flex;
+        flex-direction: column;
+        gap: 32px;
+        width: 100%;
+
+        .box-wrapper {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: $color-wit;
+            border-radius: 20px;
+            text-align: center;
+            box-shadow: 2px -4px 4px #00000042;
+
+
+            .box {
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+                padding: 32px;
+            }
+        }
+
+        .letter {
+        border-bottom: 3px solid $color-wit;
+        display: inline-flex;
+        font-size: 30px;
         align-items: center;
         justify-content: center;
-        background-color: $color-wit;
-        border-radius: 20px;
-        text-align: center;
-        box-shadow: 2px -4px 4px #00000042;
-
-
-        .box {
-            display: flex;
-            flex-direction: column;
-            gap: 16px;
-            padding: 32px;
+        margin: 0 3px;
+        height: 50px;
+        width: 20px;
         }
     }
-
-    .letter {
-    border-bottom: 3px solid #2980b9;
-    display: inline-flex;
-    font-size: 30px;
-    align-items: center;
-    justify-content: center;
-    margin: 0 3px;
-    height: 50px;
-    width: 20px;
-    }
 }
+
+// span{
+//     display: inline-block;
+//     width: 50px;
+//     height: 10px;
+//     background-color: #2980b9;
+// }
 </style>
