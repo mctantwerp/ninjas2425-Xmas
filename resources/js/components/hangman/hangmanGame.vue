ef7b0165befaf3b29b1fb388d77192865e9a6ebc
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-11-27 20:10:10 +0100

    added input field

--- resources/js/components/hangman/hangmanGame.vue
+++ resources/js/components/hangman/hangmanGame.vue
@@ -5,30 +5,28 @@
         </div>
     </div>
     <div class="word">
-        <span 
-            class="letter" 
-            :class="{ 'correct': correctLetters[i] && correctLetters[i] !== ' ' }"
-            v-for="(letter, i) in letters" 
-            :key="i">
+        <span class="letter" :class="{ 'correct': correctLetters[i] && correctLetters[i] !== ' ' }"
+            v-for="(letter, i) in letters" :key="i">
             {{ correctLetters[i] }}
         </span>
     </div>
+    <input type="text">
 </template>
 
 <script>
-    export default {
-        props: {
-        
+export default {
+    props: {
+
         letters: {
             type: Array,
             default: () => ['c', 'h', 'r', 'i', 's', 't', 'm', 'a', 's']
         }
-        
+
     },
-    data(){
-        return{
+    data() {
+        return {
             amountOfWrongTries: 0,
-            correctLetters: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 
+            correctLetters: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         }
     }
 }
@@ -64,10 +62,10 @@
     margin: 0 3px;
     height: 50px;
     width: 20px;
-        &.correct{
-            border-bottom: none;
-            color: $color-wit;
-        }
-    }
 
+    &.correct {
+        border-bottom: none;
+        color: $color-wit;
+    }
+}
 </style>
