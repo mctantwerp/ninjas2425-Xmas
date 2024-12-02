99669cfba078fc3b984be754320670a8a2e94193
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-02 22:41:48 +0100

    changed 2x2 of games layout --> responsiveness

--- resources/js/components/squareGameOverview.vue
+++ resources/js/components/squareGameOverview.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="game-container">
-        <a href="/rebus">
-            <div class="box top-left">
-                <img src="./assets/nav-cup.png" alt="">
+        <a href="/rebus" class="box">
+            <div class="top-left">
+                <img src="./assets/nav-cup.png" alt="christmas coffee cup">
                 <h2>Rebus</h2>
             </div>
         </a>
-        <a href="/tree">
-            <div class="box top-right">
-                <img src="./assets/nav-tree.png" alt="">
+        <a href="/tree" class="box">
+            <div class="top-right">
+                <img src="./assets/nav-tree.png" alt="christmas tree">
                 <h2>Tree</h2>
             </div>
         </a>
-        <a href="/hangman">
-            <div class="box bottom-left">
-                <img src="./assets/nav-ginger.png" alt="">
+        <a href="/hangman" class="box">
+            <div class="bottom-left">
+                <img src="./assets/nav-ginger.png" alt="christmas gingerman">
                 <h2>Hangman</h2>
             </div>
         </a>
-        <a href="/search">
-            <div class="box bottom-right">
-                <img src="./assets/nav-present.png" alt="">
+        <a href="/search" class="box">
+            <div class="bottom-right">
+                <img src="./assets/nav-present.png" alt="christmas present">
                 <h2>Search</h2>
             </div>
         </a>
@@ -40,27 +40,27 @@
     grid-template-rows: 1fr 1fr;
     gap: 10px;
     margin-bottom: 128px;
-}
+    width: 100%;
 
-.box {
-    padding: 20px;
-    border: 1px solid #000;
-    text-align: center;
-    font-size: 1.2rem;
-    font-weight: bold;
-    border-radius: 8px;
-    height: 130px;
-    width: 150px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
+    .box {
+        padding: 20px;
+        text-align: center;
+        font-size: 1.2rem;
+        font-weight: bold;
+        border-radius: 8px;
+        height: 130px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        background-color: $color-turquoise-20;
 
-    img {
-        width: 35px;
-        height: 35px;
+        img {
+            width: 35px;
+            height: 35px;
+        }
     }
 }
 
@@ -75,7 +75,5 @@
 .top-left,
 .top-right,
 .bottom-left,
-.bottom-right {
-    background-color: $color-turquoise-20;
-}
+.bottom-right {}
 </style>
