fc8ba5e2f146673997350badde72d9c770b5ec5e
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-03 17:14:22 +0100

    word now only shows if user completed game

--- resources/js/components/squareGameOverview.vue
+++ resources/js/components/squareGameOverview.vue
@@ -4,7 +4,7 @@
             <div class="top-left" :class="{ 'solved': rebusSolved }">
                 <img src="./assets/nav-cup.png" alt="christmas coffee cup">
                 <h2>Rebus</h2>
-                <span>Happy</span>
+                <span v-if="rebusSolved">Happy</span>
                 <span class="status-box solved" v-if="rebusSolved">Solved</span>
             </div>
         </a>
@@ -12,7 +12,7 @@
             <div class="top-right" :class="{ 'solved': treeSolved }">
                 <img src="./assets/nav-tree.png" alt="christmas tree">
                 <h2>Tree</h2>
-                <span>From</span>
+                <span v-if="treeSolved">From</span>
                 <span class="status-box solved" v-if="treeSolved">Solved</span>
             </div>
         </a>
@@ -20,7 +20,7 @@
             <div class="bottom-left" :class="{ 'solved': hangmanSolved }">
                 <img src="./assets/nav-ginger.png" alt="christmas gingerman">
                 <h2>Hangman</h2>
-                <span>Christmas</span>
+                <span v-if="hangmanSolved">Christmas</span>
                 <span class="status-box solved" v-if="hangmanSolved">Solved</span>
             </div>
         </a>
@@ -28,7 +28,7 @@
             <div class="bottom-right" :class="{ 'solved': searchSolved }">
                 <img src="./assets/nav-present.png" alt="christmas present">
                 <h2>Search</h2>
-                <span>MCT</span>
+                <span v-if="searchSolved">MCT</span>
                 <span class="status-box solved" v-if="searchSolved">Solved</span>
             </div>
         </a>
