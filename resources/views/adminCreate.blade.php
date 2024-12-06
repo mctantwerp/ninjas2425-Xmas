ae52c089ebd98eaba4e641fcf35deb598ed2f450
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-06 23:40:43 +0100

    added navbar to admin pages

--- resources/views/adminCreate.blade.php
+++ resources/views/adminCreate.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'XMAS | Admin-Create')
 
+@section('navbar')
+    <navbar navbar-title='Admin Create'></navbar>
+@endsection
+
 @section('content')
     <create-admin-page></create-admin-page>
 @endsection
--- resources/views/adminLogin.blade.php
+++ resources/views/adminLogin.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'XMAS | Admin')
 
+@section('navbar')
+    <navbar navbar-title='Admin Login'></navbar>
+@endsection
+
 @section('content')
     <admin-page></admin-page>
 @endsection
