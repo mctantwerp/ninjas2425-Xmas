07bf622c75852ecf948dd45f8082b197e28b1aae
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-14 00:43:35 +0100

    added cookie popup with necessary functionality (only enabling hotjar if user gives permission) also added animations, changed UI

---
+++ resources/js/components/CookiePopup.vue
@@ -0,0 +1,70 @@
+<template>
+    <div v-if="hasMounted">
+        <transition name="fade" mode="out-in">
+            <vue-cookie-comply v-if="!userHasAcceptedCookies" position="bottom" accept-button-text="Accept"
+                decline-button-text="Decline" background-color="#f5f5f5" text-color="#333" button-color="#007bff"
+                button-text-color="#fff" target="#app" :preferences="preferencesCookies"
+                @on-accept-all-cookies="onAccept" @on-save-cookie-preferences="onSavePreferences">
+            </vue-cookie-comply>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            preferencesCookies: [
+                {
+                    title: 'Functional Cookies',
+                    description:
+                        'Functional cookies are necessary for the website to function correctly.',
+                    items: [{ label: 'Game Rememberer', value: 'Functional', isRequired: true }],
+                },
+                {
+                    title: 'Analytics',
+                    description:
+                        'The analytics cookies will be used to improve the user experience. With this data, we can make adjustments to the website to make your experience even better!',
+                    items: [
+                        { label: 'Hotjar - Track User Flow', value: 'hotjar', isEnable: true },
+                    ],
+                },
+            ],
+            userHasAcceptedCookies: false,
+            hasMounted: false,
+        }
+    },
+    methods: {
+        onAccept() {
+            window.dispatchEvent(new CustomEvent('cookies-accepted'));
+            this.userHasAcceptedCookies = true;
+        },
+        onSavePreferences(preferences) {
+            if (preferences[1] === 'hotjar') {
+                window.dispatchEvent(new CustomEvent('hotjar-accepted-true'));
+                this.userHasAcceptedCookies = true;
+            }
+        },
+    },
+    mounted() {
+        this.hasMounted = true;
+        let rawCookieComply = localStorage.getItem('cookie-comply');
+        if (rawCookieComply !== null) {
+            this.userHasAcceptedCookies = true;
+        }
+    },
+};
+</script>
+
+
+
+<style>
+@import '../../../node_modules/vue-cookie-comply/dist/style.css';
+
+/* .cookie-wrapper {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 9999;
+} */
+</style>
--- resources/js/gsap.js
+++ resources/js/gsap.js
@@ -8,5 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         onComplete: () => {
             gsap.set(".content", { clearProps: "all" });
         }
-    })
+    });
+    //check if cookie comply exists --> because it wont exist if user has already given consent
+    if (document.querySelector('.cookie-comply')) {
+        gsap.from('.cookie-comply', {
+            opacity: 0,
+            duration: 1.5,
+            // y: -50,
+            x: -200,
+            ease: "power3.out",
+            onComplete: () => {
+                gsap.set('.cookie-comply', { clearProps: "all" });
+            }
+        });
+    }
 });
