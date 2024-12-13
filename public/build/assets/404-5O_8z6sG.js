dd198c8f3447baf1641f472c3adff7bc370e83bb
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-14 00:44:23 +0100

    new buiild

--- public/build/assets/404-5O_8z6sG.js
+++
@@ -1,1 +0,0 @@
-import{p as o,s as a}from"./snowFlakes-CvQroZTu.js";const p=o({});p.component("snow-flakes",a);p.mount(".error-page-wrapper");
---
+++ public/build/assets/404-CwWAL_sn.js
@@ -0,0 +1,1 @@
+import{m as p,s as a}from"./snowFlakes-BUT6CAsU.js";const o=p({});o.component("snow-flakes",a);o.mount(".error-page-wrapper");
