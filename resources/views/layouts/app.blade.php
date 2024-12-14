fe2c874e98d757044056e1d8794141fc9b4af6db
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-14 12:34:52 +0100

    changed location of hotjar script to opening of head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,48 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="XMAS 2024 is a festive project crafted by the final-year MCT students of KdG. Challenge yourself with four exciting games, celebrate the holiday spirit, and win fantastic prizes!">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Default Title')</title>
-    @vite('resources/sass/app.scss')
-    @yield('scss')
-    <script src="https://kit.fontawesome.com/26d2910850.js" crossorigin="anonymous" defer></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-    <!-- Standard Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-
-    <!-- Apple Touch Icon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-
-    <!-- Android Chrome -->
-    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png">
-
-    <!-- Open Graph Meta Tags -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://happyholidays.mctantwerp.be">
-    <meta property="og:title" content="XMAS 2024 | MCT Antwerp">
-    <meta property="og:description"
-        content="Discover the excitement of XMAS 2024! Find hidden posters, take on fun challenges, and win amazing prizes. Dive into a festive adventure brought to you by the last year innovative MCT students of KdG. Perfect for spreading holiday cheer and showcasing creativity during the most wonderful time of the year!">
-    <meta property="og:image" content="{{ asset('og-image.png') }}">
-    <meta property="og:image:alt"
-        content="A picture of two hands with a phone showcasing the app. Along with regular branding.">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="630">
-
-    <!-- Fonts -->
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Borel&display=swap" as="style"
-        onload="this.rel='stylesheet'">
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Braah+One&display=swap" as="style"
-        onload="this.rel='stylesheet'">
-    <link rel="preload"
-        href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap"
-        as="style" onload="this.rel='stylesheet'">
     <!-- Hotjar Tracking Code for https://happyholidays.mctantwerp.be/ -->
     <script>
         enableHotjar();
@@ -113,6 +71,48 @@
             }
         }
     </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="XMAS 2024 is a festive project crafted by the final-year MCT students of KdG. Challenge yourself with four exciting games, celebrate the holiday spirit, and win fantastic prizes!">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Default Title')</title>
+    @vite('resources/sass/app.scss')
+    @yield('scss')
+    <script src="https://kit.fontawesome.com/26d2910850.js" crossorigin="anonymous" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <!-- Standard Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+    <!-- Apple Touch Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+
+    <!-- Android Chrome -->
+    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png">
+
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://happyholidays.mctantwerp.be">
+    <meta property="og:title" content="XMAS 2024 | MCT Antwerp">
+    <meta property="og:description"
+        content="Discover the excitement of XMAS 2024! Find hidden posters, take on fun challenges, and win amazing prizes. Dive into a festive adventure brought to you by the last year innovative MCT students of KdG. Perfect for spreading holiday cheer and showcasing creativity during the most wonderful time of the year!">
+    <meta property="og:image" content="{{ asset('og-image.png') }}">
+    <meta property="og:image:alt"
+        content="A picture of two hands with a phone showcasing the app. Along with regular branding.">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+
+    <!-- Fonts -->
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Borel&display=swap" as="style"
+        onload="this.rel='stylesheet'">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Braah+One&display=swap" as="style"
+        onload="this.rel='stylesheet'">
+    <link rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap"
+        as="style" onload="this.rel='stylesheet'">
     <!-- Cookie Free Analytics -->
     <script defer src="https://cloud.umami.is/script.js" data-website-id="777bf68f-180f-42cc-8cae-15176bc505ee"></script>
 
